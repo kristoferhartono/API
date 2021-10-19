7a0e3012f250630299c0e2f326ff907d0bf6c22f
kristoferhartono <kristoferhartono18@gmail.com>
2021-10-19 22:53:31 +0700

    final

---
+++ auth/auth_handler.py
@@ -0,0 +1,41 @@
+import time
+from typing import Dict
+
+import jwt
+from decouple import config
+
+from passlib.context import CryptContext
+
+JWT_SECRET = config("secret")
+JWT_ALGORITHM = config("algorithm")
+
+
+def token_response(token: str):
+    return {
+        "access_token": token
+    }
+
+def signJWT(user_id: str) -> Dict[str, str]:
+    payload = {
+        "user_id": user_id,
+        "expires": time.time() + 600
+    }
+    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+    return token_response(token)    
+
+def decodeJWT(token: str) -> dict:
+    try:
+        decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        return decoded_token if decoded_token["expires"] >= time.time() else None
+    except:
+        return {} 
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
--- main.py
+++ main.py
@@ -1,21 +1,44 @@
 import json
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
- 
- 
+from fastapi import FastAPI, HTTPException, Depends
+
+from auth.auth_handler import signJWT, get_password_hash, verify_password
+from auth.auth_bearer import JWTBearer
+
 with open("menu.json", "r") as read_file:
     data = json.load(read_file)
+with open("user.json", "r") as read_user:
+    data2 = json.load(read_user)
+
 app = FastAPI()
+
+@app.post('/user/register')
+async def register(username : str, password : str):
+    hashedpw = get_password_hash(password)
+    credential = {'username': username, 'password' : hashedpw}
+    data2.append(credential)
+    with open("user.json", "w") as write_file:
+        json.dump(data2, write_file)
+    write_file.close()
+    return data2
+
+@app.post('/user/login')
+async def login(username : str, password : str):
+    for data_user in data2:
+        if data_user['username'] == username:
+            if verify_password(password, data_user['password']):
+                return signJWT(username)
+            else :
+                return('login failed')
+
 @app.get('/')
 def root():
     return{'Menu': 'Item'}
 
-@app.get('/menu')
+@app.get('/menu',dependencies=[Depends(JWTBearer())])
 async def read_all_menu():
     return data
 
-@app.get('/menu/{item_id}')
+@app.get('/menu/{item_id}',dependencies=[Depends(JWTBearer())])
 async def read_menu(item_id: int):
     for menu_item in data['menu']:
         if menu_item['id'] == item_id:
@@ -24,7 +47,7 @@ async def read_menu(item_id: int):
         status_code=1, detail=f'Item not found'
     )
 
-@app.post('/menu')
+@app.post('/menu',dependencies=[Depends(JWTBearer())])
 async def add_menu(name: str):
     id = 1
     if (len(data['menu']) > 0):
@@ -41,7 +64,7 @@ async def add_menu(name: str):
         status_code = 2, detail=f'Internal Server Error'
     )
 
-@app.put('/menu/{item_id}')
+@app.put('/menu/{item_id}',dependencies=[Depends(JWTBearer())])
 async def update_menu(item_id: int, name: str):
     for menu_item in data['menu']:
         if menu_item['id'] == item_id:
@@ -58,7 +81,7 @@ async def update_menu(item_id: int, name: str):
         status_code=3, detail=f'Item not found'
     )
 
-@app.delete('/menu/{item_id}')
+@app.delete('/menu/{item_id}',dependencies=[Depends(JWTBearer())])
 async def delete_menu(item_id: int):
     for menu_item in data['menu']:
         if menu_item['id'] == item_id:
